8c8821f516d440f81871e1449562efa5bea1f8ed
User 1 <user1@CathyKigsiPhone.localdomain>
2019-09-26 19:35:30 +0100

    Task 23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const path = require('path');
+const port = process.env.PORT || 3002;
 const getShows = require('./shows');
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(port, () => {
 console.log(`App listening on port ${port}!`);
+});
+
+app.use(express.static(path.join(__dirname, '../../build')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../../build', 'index.html'));
 });
